783d1dd1fd107e90ab1007ce2352d471d1cc983d
tiennguyen <neit@tiennguyens-MacBook-Pro.local>
2016-12-13 14:53:12 +0700

    add button chat for fireChat

--- myFirstSwift/FireChatViewController.swift
+++ myFirstSwift/FireChatViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 tiennguyen. All rights reserved.
 //
 
+import Photos
 import UIKit
 import SnapKit
 import Firebase
@@ -17,6 +18,9 @@ UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDele
     var messages: [FIRDataSnapshot]! = []
     var msglength: NSNumber = 10
     fileprivate var _refHandle: FIRDatabaseHandle!
+    
+    var storageRef: FIRStorageReference!
+    var remoteConfig: FIRRemoteConfig!
     
     private let tableView: UITableView = {
         let tableView = UITableView(frame: CGRect.zero, style: .plain)
@@ -38,6 +42,13 @@ UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDele
         button.setTitleColor(UIColor.white, for: UIControlState.normal)
         button.backgroundColor = UIColor.blue
         button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    private let btnAddPhoto: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "ic_add_a_photo"), for: .normal)
+        button.backgroundColor = UIColor.clear
         return button
     }()
     
@@ -77,11 +88,19 @@ UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDele
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
+        self.view.addSubview(btnAddPhoto)
+        btnAddPhoto.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.tableView.snp.bottom).offset(22)
+            make.left.equalTo(self.view).offset(33)
+            make.width.height.equalTo(33)
+        }
+        btnAddPhoto.addTarget(self, action: #selector(didTapAddPhoto(_:)), for: .touchUpInside)
+        
         self.view.addSubview(txtMessage)
         txtMessage.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(self.tableView.snp.bottom).offset(22)
-            make.left.equalTo(self.view).offset(33)
-            make.right.equalTo(self.view).offset(-133)
+            make.left.equalTo(self.btnAddPhoto.snp.right).offset(8)
+            make.right.equalTo(self.view).offset(-100)
             make.height.equalTo(33)
         }
         txtMessage.delegate = self
@@ -89,7 +108,7 @@ UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDele
         self.view.addSubview(btnSend)
         btnSend.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(self.tableView.snp.bottom).offset(22)
-            make.left.equalTo(self.txtMessage.snp.right).offset(11)
+            make.left.equalTo(self.txtMessage.snp.right).offset(8)
             make.right.equalTo(self.view).offset(-33)
             make.height.equalTo(33)
         }
@@ -118,6 +137,8 @@ UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDele
     }
     
     func configureStorage() {
+        let storageUrl = FIRApp.defaultApp()?.options.storageBucket
+        storageRef = FIRStorage.storage().reference(forURL: "gs://" + storageUrl!)
     }
     
     func configureRemoteConfig() {
@@ -183,6 +204,54 @@ UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDele
         }
         // Push data to Firebase Database
         self.ref.child("messages").childByAutoId().setValue(mdata)
+    }
+    
+    // add Photo
+    func didTapAddPhoto(_ sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)) {
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+        } else {
+            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        }
+        
+        present(picker, animated: true, completion:nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion:nil)
+        guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
+        
+        // if it's a photo from the library, not an image from the camera
+        if #available(iOS 8.0, *), let referenceURL = info[UIImagePickerControllerReferenceURL] {
+            let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceURL as! URL], options: nil)
+            let asset = assets.firstObject
+            asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
+                let imageFile = contentEditingInput?.fullSizeImageURL
+                let filePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\((referenceURL as AnyObject).lastPathComponent!)"
+            })
+        } else {
+            guard let image = info[UIImagePickerControllerOriginalImage] as! UIImage? else { return }
+            let imageData = UIImageJPEGRepresentation(image, 0.8)
+            guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
+            let imagePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion:nil)
+    }
+    
+    func showAlert(withTitle title:String, message:String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title,
+                                          message: message, preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "Dismiss", style: .destructive, handler: nil)
+            alert.addAction(dismissAction)
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
 }
